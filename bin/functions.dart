c4e0fba74225e31b949e29ffd75199bed3b0a5bb
Mamoun2020 <mmun2019@gmail.com>
2022-06-23 09:16:08 +0300

    function ex

---
+++ bin/functions.dart
@@ -0,0 +1,44 @@
+//generic function
+typedef Name(String name);
+// defining fullNamed function to use it in typedef to pass value
+  fullName(String fullName){
+    print(fullName);
+  }
+//enumeration data type
+enum UserType {admin,client,seller}
+void main(){
+  // call generic (typedef) function
+  Name youName = fullName;
+  youName('mamoun');
+  /*
+  now,
+  * there are 4 type of parameter in dart to pass value to functions
+  * 1. positional, 2.positional named, 3.Positional optional 4. Named optional
+  * */
+  //3.
+  // String yourInfo(String name,[int age=22]){
+  //   return name +'\n$age';
+  // }
+  // to call it yourInfo('mamoun',25) or yourInfo('mamoun')
+
+  //4.
+  // String yourInfo(String name,{int age=22}){
+  //   return name +'\n$age';
+  // }
+  //or
+  // String yourInfo(String name,{required int age}){
+  //   return name +'\n$age';
+  // }
+  // to call it yourInfo('mamoun',age:22)
+
+  checkUser(UserType user){
+    if(user == UserType.admin){
+        print('hello,admin');
+    }else if (user == UserType.client){
+        print('hello,admin');
+    }else{
+          print('hello,admin');
+    }
+  }
+  checkUser(UserType.admin);
+}
