fa21b4c9e5562e37271c30b6c174c9521771b95c
Mamoun2020 <mmun2019@gmail.com>
2022-06-23 09:33:09 +0300

    functions ex2

--- bin/functions.dart
+++ bin/functions.dart
@@ -13,15 +13,15 @@ void main(){
   /*
   now,
   * there are 4 type of parameter in dart to pass value to functions
-  * 1. positional, 2.positional named, 3.Positional optional 4. Named optional
+  * 1. Positional optional 2. Named optional
   * */
-  //3.
+  //1.
   // String yourInfo(String name,[int age=22]){
   //   return name +'\n$age';
   // }
   // to call it yourInfo('mamoun',25) or yourInfo('mamoun')
 
-  //4.
+  //2.
   // String yourInfo(String name,{int age=22}){
   //   return name +'\n$age';
   // }
@@ -38,6 +38,7 @@ void main(){
   * 3. parameters
   * 4. implementation
   * */
+  // to make a check user from select option from enum data type
   checkUser(UserType user){
     if(user == UserType.admin){
         print('hello,admin');
@@ -47,8 +48,31 @@ void main(){
           print('hello,admin');
     }
   }
+  // print hello admin
   checkUser(UserType.admin);
 
   // there are lambda function to make return in one statement
   // int sum(int i, int y) => i +y;
+
+  // example for type of functions parameter
+  // int Multi(int x,[int y]){
+  //   return x * y;
+  // }
+  // null safety problem and we can solve it
+  // first solution
+  // int Multi(int x,[int y = 1]){
+  //   return x * y;
+  // }
+  //second solution
+  // int Multi(int x,[int? y]){
+  //   return x * (y ?? 1);
+  // }
+  // third solution
+  int multi([int x = 5,int? y]){
+    if(y == null){
+      return x * 1;
+    }else{
+      return x * y;
+    }
+  }
 }
